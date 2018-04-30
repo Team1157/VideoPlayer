e33d943315c1b96d921ef77347212e2dd2b4a085
Tobias Messner <tobias.d.messner@gmail.com>
2018-04-30 06:54:36 -0600

    Fixed link

--- update.sh
+++ update.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo rm main.py
-sudo wget https://gist.githubusercontent.com/ZusorCode/b739c6491b99e5f906af668726364aed/raw/b5c14fd8518b21ce81f6838b8d8cd33ebf92d928/main.py
+sudo wget https://github.com/ZusorCode/VideoPlayer/blob/master/main.py
