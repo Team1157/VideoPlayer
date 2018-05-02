086624a90b7cb09dc5df94e71a508eb992b0d59d
Tobias Messner <tobias.d.messner@gmail.com>
2018-05-02 14:01:01 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install python3 python3-pip libzbar0 curl -y
+sudo apt-get install python3 python3-pip libzbar0 curl omxplayer -y
 pip3 install --upgrade setuptools wheel
 pip3 install opencv-python pyzbar
+sudo pip3 install --upgrade setuptools wheel
+sudo pip3 install opencv-python pyzbar
+mkdir ./Videos
